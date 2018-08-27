13ba19c43db7bc6cc44c65160b9f42464e687c42
Katy <ihatetoast@gmail.com>
2018-08-27 14:40:17 -0500

    user can like a note and unlike

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -89,6 +89,132 @@ router.post(
   }
 );
 
+/////////////////
+/////////////////
+//   LIKES     //
+/////////////////
+/////////////////
+
+// @route   POST api/notes/like/:id (id of note)
+// @desc    like a note
+// @access  Private
+
+router.post(
+  '/like/:id',
+  passport.authenticate('jwt', { session: false }),
+  (request, response) => {
+    //get teh user
+    Profile.findOne({ user: request.user.id })
+      .then(() => {
+        //get the post by id
+        Note.findById(request.params.id).then(note => {
+          //check if user's aready liked. is id already there?
+          if (
+            note.likes.filter(like => like.user.toString() === request.user.id)
+              .length > 0
+          ) {
+            return response
+              .status(400)
+              .json({ alreadyLiked: 'User already liked this note' });
+          }
+          //if not already liked, add user's id to likes arr
+          note.likes.unshift({ user: request.user.id });
+          //save to db
+          note.save().then(note => response.json(note));
+        });
+      })
+      .catch(err =>
+        response
+          .status(404)
+          .json({ nonote: 'Cannot delete. No post with given id found.' })
+      );
+  }
+);
+// NOTE NOTE  NOTE NOTE  NOTE NOTE  NOTE NOTE  NOTE NOTE
+//unliking is a post request and not a delete.
+//I am posting from front end and just using JS array methods (index of, splice) to deal with removing a user's id from the likes arr.
+
+// @route   POST api/notes/unlike/:id (id of NOTE)
+// @desc    like a note
+// @access  Private
+
+router.post(
+  '/unlike/:id',
+  passport.authenticate('jwt', { session: false }),
+  (request, response) => {
+    //get teh user
+    Profile.findOne({ user: request.user.id })
+      .then(() => {
+        //get the post by id
+        Note.findById(request.params.id).then(note => {
+          //check if user's aready liked. is id already there?
+          if (
+            note.likes.filter(like => like.user.toString() === request.user.id)
+              .length === 0
+          ) {
+            return response.status(400).json({
+              notliked: 'User cannot unlike bc user has not yet liked'
+            });
+          }
+          //index of note to unlike in array.map over to find idx of like that matches request.user.id
+          // 1 map over the array and convert every elem.user to string
+          // 2 use indexof with req.us.id to find the index
+          // 3 use likes array and found index to splice and remove one
+          const idxOfNoteToRem = note.likes
+            .map(elem => elem.user.toString())
+            .indexOf(request.user.id);
+          note.likes.splice(idxOfNoteToRem, 1);
+          //save adjusted arr to db
+          note.save().then(note => response.json(note));
+        });
+      })
+      .catch(err =>
+        response
+          .status(404)
+          .json({ nonote: 'Cannot delete. No post with given id found.' })
+      );
+  }
+);
+/////////////////
+/////////////////
+//   COMMENTS  //
+/////////////////
+/////////////////
+// @route   POST api/notes/comment/:id (id of note)
+// @desc    comment on a note
+// @access  Private
+
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (request, response) => {
+    //validation: same as note bc just about text
+    const { errors, isValid } = validateNoteInput(request.body);
+
+    if (!isValid) {
+      //400 status
+      return response.status(400).json(errors);
+    }
+    //if all good, find the note with the id
+    Note.findById(request.params.id)
+      .then(note => {
+        //make new comment (is obj)
+        const newComment = {
+          user: request.user.id,
+          text: request.body.text,
+          name: request.body.name,
+          avatar: request.body.avatar
+        };
+        //like likes. add to arr with unshift
+        note.comments.unshift(newComment);
+        note.save().then(note => response.json(note));
+      })
+      .catch(err =>
+        response.status(404).json({ notenotfound: 'No note found.' })
+      );
+  }
+);
+
 //////////////////////////////////////////////
 //////////////////////////////////////////////
 //     DELETE DELETE DELETE DELETE          //
@@ -108,7 +234,7 @@ router.delete(
     Profile.findOne({ user: request.user.id })
       .then(() => {
         //get the post by id
-        Note.findById({ _id: request.params.id }).then(note => {
+        Note.findById(request.params.id).then(note => {
           //make sure the owner is the one we want
           if (note.user.toString() !== request.user.id) {
             return response
