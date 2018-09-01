0a3f4f3a4ccb2bfe6b358e987318282e5d0d0356
Katy <ihatetoast@gmail.com>
2018-08-31 22:48:52 -0500

    react-scripts fixed. router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Nav from './components/layout/Nav/Nav';
 import Landing from './components/layout/Landing/Landing';
 import Footer from './components/layout/Footer/Footer';
@@ -8,11 +9,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-        <Nav />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div>
+          <Nav />
+          <Route exact path="/" component={Landing} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
